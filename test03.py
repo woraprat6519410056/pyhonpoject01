ed49e71e6cb71148ecf0841b97d18730196c4049
woraprat6519410056 <s6519410056@sau.ac.th>
2023-08-18 11:33:01 +0700

    code v.1 : creat 3 file

---
+++ test03.py
@@ -0,0 +1,22 @@
+print('AAA'); print('DDD')
+print('BBB')
+print('CCC')
+print('DDD\nEEE\nFFF')
+print('DDD\\EEE\\FFF')
+#print('DDD\\\\EEE\\\\FFF')
+#-----------------------------
+print('DDD\tEEE\tFFF')
+print('Welcome "SAU"')
+print("Welcome \"SAU\"")
+print("Welcome 'IOT'")
+#-----------------------------
+print("Welcome 'IOT'")
+print('Welcome \'IOT\'')
+print('ABC\\DFG')
+#-------------------------------
+print('''
+      SAU...
+      IoT...
+      Wow...
+      Hi...
+      ''')
